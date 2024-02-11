f77dfeba43a5cc2c87c05fc4bbd172031a8c9f0a
maayan <maayansh.ms@gmail.com>
2024-02-11 15:20:20 +0200

    added all api functions using axios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,6 @@
 import {ApplicationConfig} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {provideState, provideStore} from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
-import {PublishersEffects} from "./store/publishers.effects";
-import {publishersReducer} from "./store/publishers.reducers";
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,3 @@
 import { Routes } from '@angular/router';
-import {provideState} from "@ngrx/store";
-import {publishersReducer} from "./store/publishers.reducers";
 
 export const routes: Routes = [];
